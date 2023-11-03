3b752c47877c96b584ab14fd96e4924157d50081
Amanda Simmons <carpe737diem@gmail.com>
2023-11-03 13:04:03 -0400

    Added functionslity to StarRating function to reset hover and rating onDoubleclick based on a comment I read on the documented site.

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
 
+// Implemented this code snippet by "morskibg"from the comments of https://dev.to/michaelburrows/create-a-custom-react-star-rating-component-5o6 
+// in order to reset rating and hover upon double click.
+
+// morskibg
+
+// Hi, good job ! Just to suggest option to reset rating back to 0 thru dbl click on any star :
+// onDoubleClick={() => {
+// setRating(0);
+// setHover(0);
+// }}
+
 const StarRating = () => {
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
@@ -15,6 +26,8 @@ const StarRating = () => {
              onClick={() => setRating(index)}
              onMouseEnter={() => setHover(index)}
              onMouseLeave={() => setHover(rating)}
+             onDoubleClick={() => {setRating(0);
+                                   setHover(0);}}
             >
              <span className="star">&#9733;</span>
             </button>  
